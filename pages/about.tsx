28f0a855581f92a3411e7cffa53b15322ffc4547
Zhenghao He <iron4wine@outlook.com>
2021-12-22 12:22:04 -0800

    about me

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,7 @@ export default function About() {
   return (
     <div className="max-w-[75ch] mx-auto py-12 px-5 flex flex-col gap-8 justify-center items-center">
 
-      <p>this is a page lol</p>
+      <p>this is a page lol lol</p>
     </div>
   );
 }
