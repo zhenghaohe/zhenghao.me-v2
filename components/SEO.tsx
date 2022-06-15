7049f741166bcf087378aace6d3de729b9349f3e
Zhenghao He <iron4wine@outlook.com>
2022-06-14 18:05:35 -0700

    fix

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -3,6 +3,8 @@ import { DefaultSeo } from 'next-seo';
 export const SEO: React.FC = () => {
   return (
     <DefaultSeo
+    title="Zhenghao's site"
+    description="The official site of Zhenghao He, a software engineer and a TypeScript/JavaScript enthusiast."
       openGraph={{
         site_name: 'zhenghao.io'
       }}
