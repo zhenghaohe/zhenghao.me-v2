0caa1e00289c7117417259d6a9af1e868f6455d8
Zhenghao He <iron4wine@outlook.com>
2024-02-18 14:01:01 -0800

    update experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -4,15 +4,36 @@ import { a11yDate, visibleDate } from '@/utils/date';
 
 const experience = [
   {
+    company: 'DocuSign',
+    location: 'Seattle',
+    position: 'Team Lead',
+    startDate: 'Mar, 2023',
+    endDate: 'present',
+    details: (
+      <>
+        <p>
+          Joined the Product-Led Growth e-commerce team, managing a team of 6 engineers, delivering
+          vital, friction-less purchasing experiences for DocuSign customers across our self-serve
+          eco-system.
+        </p>
+        <p>
+          Tech stack: Next.js, React, Node.js, TypeScript, Tailwind CSS, Webpack, Playwright,
+          Optimizely, GraphQL, Apollo Client, REST APIs, Azure, Contentful
+        </p>
+      </>
+    )
+  },
+  {
     company: 'Instacart',
     location: 'Seattle',
     position: 'Senior Software Engineer',
     startDate: 'June, 2021',
-    endDate: 'Dec, 2022',
+    endDate: 'Jan, 2023',
     details: (
       <>
         <p>
-          Building an enterprise dashboard where the retailers manage their catalogs, pick-up
+          Joined the pick-up team as the frontend tech lead and worked on the Retailer Platform
+          Portal - an enterprise dashboard where the retailers manage their catalogs, pick-up
           staffing and scheduling, as well as to view analytics around various business metrics.
         </p>
         <p>Tech stack: React, TypeScript, GraphQL, Apollo, Ruby on Rails</p>
@@ -28,9 +49,8 @@ const experience = [
     details: (
       <>
         <p>
-          Worked on AWS IoT Console, migrating a legacy Angular monolithic codebase into multiple
-          React packages owned by separate service teams with split pipelines and deployment bundles
-          by adopting a micro-frontend architecture.
+          Worked on AWS IoT Console, a web platform where our customers can collect, organize, and
+          monitor data from industrial equipment at scale to make better, data-driven decisions.
         </p>
         <p>Tech stack: React, Angular, TypeScript, AWS, React Query, single-spa</p>
       </>
@@ -40,14 +60,18 @@ const experience = [
     company: 'Arista Networks',
     location: 'Vnacouver',
     position: 'Software Engineer',
-    startDate: 'Nov, 2019',
+    startDate: 'Sep, 2018',
     endDate: 'May, 2020',
-    details:
-      (<><p>Worked on CloudVision Platform, a webapp which monitors data center networks’ hierarchy and performance.</p>
-      <p>Tech stack: TypeScript, React/Redux, D3.js, WebSockets</p>
-      </>)
-  },
-
+    details: (
+      <>
+        <p>
+          Worked on CloudVision Platform, a webapp which monitors data center networks’ hierarchy
+          and performance.
+        </p>
+        <p>Tech stack: TypeScript, React/Redux, D3.js, WebSockets</p>
+      </>
+    )
+  }
 ];
 
 export function Experience() {
